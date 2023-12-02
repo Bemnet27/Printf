27c3a27b4f1e05a0d9f6542240ecd70267d53836
bemnet <bemnetabebe1@gmail.com>
2023-12-02 08:50:10 -0800

    handles c, s and % also it has betty errors

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+#endif
---
+++ printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * _printf - custom printf function handling c and s
+ * @format: format string
+ *
+ * Return: number of character
+ */
+int _printf(const char *format, ...)
+{
+	char holder;
+	int counter;
+	char *str;
+
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					holder = va_arg(args, int);
+					write(1, &holder, 1);
+					counter++;
+					break;
+				case 's':
+					str = va_arg(args, char*);
+					write(1, str, 1);
+					counter;
+					break;
+				case '%':
+					write(1, format, 1);
+					counter++;
+					break;
+				default:
+					write(1, format, 1);
+					counter++;
+					break;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			counter++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (counter);
+}
