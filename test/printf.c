65237a8830152bd091c050c25be110ec4cbfb7ce
Benson <ibejibenson2@gmail.com>
2023-12-02 15:54:26 -0800

    task 2

--- test/printf.c
+++ test/printf.c
@@ -1,10 +1,53 @@
 #include "main.h"
 /**
- * _printf - custom printf function handling c and s
+ * _printf - custom printf function handling c, s, d and i
  * @format: format string
  *
  * Return: number of character
  */
+
+int _charprintf(int holder)
+{
+	return write(1, &holder, 1);
+}
+
+void _strprintf(char *str)
+{
+	int n = strlen(str);
+	int i;
+
+	for (i = 0; (i < n) && (*str != '\0'); i++)
+	{
+		_charprintf((int)*str);
+		++str;
+	}
+}
+
+int _intprintf(int holder, int base_val)
+{
+	int counter;
+
+	char hex_values[] = "0123456789abcdef";
+	counter = 0;
+	if (holder < 0)
+	{
+		_charprintf('-');
+		counter++;
+		return _intprintf(((holder*holder)/2), base_val) + 1;
+	}
+	else if (holder < base_val)
+	{
+		counter++;
+		_charprintf(hex_values[holder]);
+		return counter;
+	}
+	else
+	{
+		counter = _intprintf(holder / base_val, base_val);
+		return counter + _intprintf(holder % base_val, base_val);
+	}
+}
+
 int _printf(const char *format, ...)
 {
 	char holder;
@@ -15,6 +58,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	counter = 0;
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -24,18 +68,25 @@ int _printf(const char *format, ...)
 			{
 				case 'c':
 					holder = va_arg(args, int);
-					write(1, &holder, 1);
-					counter++;
+					counter += _charprintf(holder);
 					break;
 				case 's':
 					str = va_arg(args, char*);
-					write(1, str, strlen(str));
-					counter++;
+					_strprintf(str);
+					counter += strlen(str);
 					break;
 				case '%':
 					write(1, format, 1);
 					counter++;
 					break;
+				case 'd':
+					holder = va_arg(args, int);
+					counter += _intprintf(holder, 10);
+					break;
+				case 'i':
+					holder = va_arg(args, int);
+                                        counter += _intprintf(holder, 10);
+                                        break;
 				default:
 					write(1, format, 1);
 					counter++;
